ef53b97c0df46b4a23476afb0b644cc4f81fb798
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-09-07 10:52:38 +0300

    project startup commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Credit Scoring Engine Business and Client app backend
+
+ ### The app runs on port 8889 for instance http://localhost:8889
+
+ ### Name your database creditscoringd
+
+### And replace password and username of database connection in application.properties accordingly.
---
+++ src/main/java/com/dxvalley/creditscoring/CreditScoringApplication.java
@@ -0,0 +1,14 @@
+package com.dxvalley.creditscoring;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditScoringApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(CreditScoringApplication.class, args);
+    }
+
+}
