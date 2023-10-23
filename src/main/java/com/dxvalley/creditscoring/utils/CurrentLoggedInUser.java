ebf5924c264f44fd26714b568c53df6578e56e61
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-10-23 13:53:01 +0300

    bug fix for current logged in user to handle email change

--- src/main/java/com/dxvalley/creditscoring/utils/CurrentLoggedInUser.java
+++ src/main/java/com/dxvalley/creditscoring/utils/CurrentLoggedInUser.java
@@ -1,5 +1,6 @@
 package com.dxvalley.creditscoring.utils;
 
+import com.dxvalley.creditscoring.exceptions.customExceptions.BannedUserException;
 import com.dxvalley.creditscoring.exceptions.customExceptions.ResourceNotFoundException;
 import com.dxvalley.creditscoring.exceptions.customExceptions.UnauthorizedException;
 import com.dxvalley.creditscoring.userManager.user.UserRepository;
@@ -21,8 +22,13 @@ public class CurrentLoggedInUser {
         if (authentication instanceof AnonymousAuthenticationToken)
             throw new UnauthorizedException("Access denied. Please provide a valid authentication token.");
 
-        return userRepository.findByUsername(authentication.getName())
-                .orElseThrow(() -> new ResourceNotFoundException("Please login with a new email and try again."));
         // If a user changes his or her email address, he or she must log in again.
+        Users user = userRepository.findByUsername(authentication.getName())
+                .orElseThrow(() -> new ResourceNotFoundException("Please login with a new email and try again."));
+
+        if (user.getUserStatus() == Status.BANNED)
+            throw new BannedUserException();
+
+        return user;
     }
 }
